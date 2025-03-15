e48db82e83726375680e62086c44b7a56debc1df
Amir Ali Anwar <amiralianwar611@gmail.com>
2025-03-15 13:31:29 +0500

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 const App = () => {
-  return <h1 className='text-2xl font-bold'>MSW Starter</h1>;
+  return <div className='max-w-3xl mx-auto mt-10 p-4'>
+  <h1 className='text-2xl font-bold mb-4'>Posts Manager</h1>
+  dasklkladks
+</div>;
 };
 export default App;
