ad8a16ed58b63a78b1fdf9c88b6065c02c22f20f
Amir Ali Anwar <amiralianwar611@gmail.com>
2025-04-22 15:45:19 +0500

    settting up the components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import { useEffect } from "react";
+import Form from "./components/Form";
+import List from "./components/List";
+import { usePosts } from "./hooks/usePost";
 const App = () => {
-  return <div className='max-w-3xl mx-auto mt-10 p-4'>
-  <h1 className='text-2xl font-bold mb-4'>Posts Manager</h1>
-  dasklkladks
-</div>;
+  const {
+    posts,
+    error,
+    fetchPosts,
+    handleCreatePost,
+    handleDeletePost,
+    handleLike,
+  } = usePosts();
+  useEffect(() => {
+
+    fetchPosts()
+    return () => {
+        
+    }
+  }, [posts])
+  
+  return (
+    <div className="max-w-3xl mx-auto mt-10 p-4">
+      <h1 className="text-2xl font-bold mb-4">Posts Manager</h1>
+      {error && <div className='text-red-500 mb-4'>{error}</div>}
+      <Form onSubmit={handleCreatePost}/>
+      <List posts={posts} onLike={handleLike} onDelete={handleDeletePost} />
+    </div>
+  );
 };
 export default App;
