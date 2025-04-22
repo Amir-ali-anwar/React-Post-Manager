1614803168d8748e5c675a63970cf2731bbb4d48
Amir Ali Anwar <amiralianwar611@gmail.com>
2025-04-22 15:16:12 +0500

    Use post hook completed

--- src/hooks/usePost.ts
+++ src/hooks/usePost.ts
@@ -13,4 +13,50 @@ const API_URL = 'http://localhost:4000/posts'
 export const usePosts = () => {
     const [posts, setPosts] = useState<Post[]>([])
     const [error, setError] = useState<string>('')
+
+    const fetchPosts = async (): Promise<void> => {
+        try {
+            const { data } = await axios(API_URL)
+            setPosts(data)
+        } catch (error) {
+            setError("Failed to Fetch the data")
+        }
+    }
+    const handleCreatePost = async (postData: Post): Promise<void> => {
+        try {
+            await axios.post(API_URL, {
+                ...postData
+            })
+        } catch (error) {
+            setError("Failed to Create the data")
+        }
+    }
+    const handleDeletePost = async (postId: postWithOutId): Promise<void> => {
+        try {
+            await axios.delete(`${API_URL}/${postId}`);
+            await fetchPosts()
+        } catch (error) {
+            setError("Failed to Delete post")
+        }
+    }
+    const handleLike = async (postId: string): Promise<void> => {
+        try {
+            const post = posts.find((post) => post.id === postId);
+            if (!post) {
+                setError('Post not found');
+                return;
+            }
+            const response = await axios.put(`${API_URL}/${postId}`, {
+                ...post,
+                likes: post.likes + 1
+            })
+            await fetchPosts()
+            setError('');
+        } catch (error) {
+            setError('Failed to like post');
+        }
+    }
+    return {
+        posts,error,fetchPosts,handleCreatePost,handleDeletePost,handleLike
+    }
 }
