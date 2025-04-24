60902d60c823dcaf3750fac02d1f8e690135cc53
Amir Ali Anwar <amiralianwar611@gmail.com>
2025-04-24 21:59:33 +0500

    writing down the handlers for mock serice request

--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -1,5 +1,5 @@
 import { http, HttpResponse } from 'msw'
-import { Post } from '../hooks/usePost'
+import { Post,postWithOutId } from '../hooks/usePost'
 const url = "http://localhost:4000/posts"
 
 let posts: Post[] = [
@@ -18,11 +18,15 @@ let posts: Post[] = [
 export const handlers = [
     // GET /posts
     http.get(url, async () => {
-        return HttpResponse.json()
+        return HttpResponse.json(posts, { status: 200 });
     }),
     // POST /posts
     http.post(url, async ({ request }) => {
-        const newPost = (await request.json()) as Post;
+        const postData = (await request.json()) as postWithOutId;
+        const newPost: Post = {
+            ...postData,
+            id: crypto.randomUUID(),
+        };
         posts.push(newPost)
         return HttpResponse.json(newPost, { status: 201 })
     }),
@@ -30,19 +34,53 @@ export const handlers = [
         const { id } = params
         const updatedPost = (await request.json()) as Post;
         const index = posts.findIndex((post) => post.id === id);
+        if(index === -1){
+            return HttpResponse.json({ message: "Post not found" }, { status: 404 })
+        }
         posts[index] = updatedPost;
         return HttpResponse.json(updatedPost, { status: 200 })
-    }),
-    http.patch(`${url}/:id`, async ({ params, request }) => {
-        const { id } = params
-        const updatedPost = (await request.json()) as Post;
-        const index = posts.findIndex((post) => post.id === id);
-        posts[index] = updatedPost;
-        return HttpResponse.json(updatedPost, { status: 200 });
     }),
     http.delete(`${url}/:id`, async ({ params }) => {
         const { id } = params;
         posts = posts.filter((post) => post.id !== id);
         return HttpResponse.json(null, { status: 200 });
     }),
+
 ]
+export const getErrorHandler = [
+    http.get(url, () => {
+        return HttpResponse.json(
+            { message: 'Failed to fetch posts' },
+            { status: 500 }
+        );
+    }),
+];
+
+export const createErrorHandler = [
+    http.post(url, () => {
+        return HttpResponse.json(
+            { message: 'Failed to create post' },
+            { status: 400 }
+        );
+    }),
+];
+
+
+export const updateErrorHandler = [
+    http.put(`${url}/:id`, () => {
+        return HttpResponse.json(
+            { message: 'Failed to update post' },
+            { status: 400 }
+        );
+    }),
+]
+
+export const deleteErrorHandler = [
+    http.delete(`${url}/:id`, () => {
+      return HttpResponse.json(
+        { message: 'Failed to delete post' },
+        { status: 400 }
+      );
+    }),
+  ];
+  
