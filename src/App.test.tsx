e3264a6073b72db75bbfb0d7e5a0002bdc002477
Amir Ali Anwar <amiralianwar611@gmail.com>
2025-04-25 15:06:09 +0500

    Writing down the handlers, server.ts and test cases

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,16 @@
-import { screen,render,within } from "@testing-library/react";
+import { screen,render,within, findByText } from "@testing-library/react";
 import App from "./App";
 import userEvent from '@testing-library/user-event'
 
-describe('App', () => {
-    test('render the App component',()=>{
-        render(<App/>)
-        expect(screen.getByText(/posts manager/i)).toBeInTheDocument();
-    })
-})
+describe("App", () => {
+  test("render the App component", () => {
+    render(<App />);
+    expect(screen.getByText(/posts manager/i)).toBeInTheDocument();
+  });
+  test('fetches posts on mount', async () => {
+    render(<App />);
+    expect(await screen.findByText(/first post/i)).toBeInTheDocument();
+    expect(await screen.findByText(/second post/i)).toBeInTheDocument();
+    // expect(await screen.findByText(/third post/i)).toBeInTheDocument();
+  });
+});
