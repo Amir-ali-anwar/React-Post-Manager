f8a4928f8a1287623ad7327adc59a62b572f980a
Amir Ali Anwar <amiralianwar611@gmail.com>
2025-04-29 12:49:57 +0500

    completed the Form Component test cases

--- src/__tests__/Form.test.tsx
+++ src/__tests__/Form.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import Form from "../components/Form";
-import userEvent, {type UserEvent } from "@testing-library/user-event";
+import userEvent, { type UserEvent } from "@testing-library/user-event";
 
 export const getFormElements = () => {
   return {
@@ -9,36 +9,40 @@ export const getFormElements = () => {
   };
 };
 
-describe('Form Component', () => {
-    const mockOnSubmit = vi.fn();
-    let user: UserEvent
-    beforeEach(()=>{
-        user= userEvent.setup()
-        mockOnSubmit.mockClear()
-        render(<Form  onSubmit={mockOnSubmit}/>)
-    })
-    test('Renders Form Elements correctly',()=>{
-        const {input,button}= getFormElements()
-        expect(input).toHaveValue('')
-        expect(button).toBeInTheDocument()
-    })
-    test('Updates Input value on change', async()=>{
-        const { input, button } = getFormElements();
-        await user.type(input,'Test Post')
-        expect(input).toHaveValue('Test Post')
-        await user.click(button)
-        expect(input).toHaveValue('')
-        
-      })  
-      test('Form Data validation after the form Submission', async()=>{
-        const { input, button } = getFormElements();
-        await user.type(input,'Test Post')
-        await user.click(button)
-        expect(mockOnSubmit).toHaveBeenCalledWith({
-          title:'Test Post',
-          likes:0
-        })
-        
-    })
+describe("Form Component", () => {
+  const mockOnSubmit = vi.fn();
+  let user: UserEvent;
+  beforeEach(() => {
+    user = userEvent.setup();
+    mockOnSubmit.mockClear();
+    render(<Form onSubmit={mockOnSubmit} />);
+  });
+  test("Renders Form Elements correctly", () => {
+    const { input, button } = getFormElements();
+    expect(input).toHaveValue("");
+    expect(button).toBeInTheDocument();
+  });
+  test("Updates Input value on change", async () => {
+    const { input, button } = getFormElements();
+    await user.type(input, "Test Post");
+    expect(input).toHaveValue("Test Post");
+    await user.click(button);
+    expect(input).toHaveValue("");
+  });
+  test("Form Data validation after the form Submission", async () => {
+    const { input, button } = getFormElements();
+    await user.type(input, "Test Post");
+    await user.click(button);
+    expect(mockOnSubmit).toHaveBeenCalledWith({
+      title: "Test Post",
+      likes: 0,
+    });
+  });
+  test('Form must have empty values after form submission', async ()=>{
+    const { input, button } = getFormElements();
+    await user.type(input, 'Test Post');
+    await user.click(button);
+    expect(input).toHaveValue('');
+  })
 
-})
+});
